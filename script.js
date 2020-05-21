d0d09f9a619d3bd6f3e13a574003e0653ec39e13
undeba@mail.ru <undeba@mail.ru>
2020-05-21 19:40:57 +0300

    новый

---
+++ script.js
@@ -0,0 +1,17 @@
+'use strict()';
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+console.log ("string"*9);
+let persone ={
+    name: "John",
+    age: 24,
+    isMarried: false
+};
+console.log(persone.name);
+let arr = ['plum.png', 'orang.jpg', 'apple.bmp'];
+console.log(arr[0]);
+let bb = confirm("кто тут у нас Оля?");
+console.log(bb);
+let ss = prompt("вы не оля?", "нет");
+console.log(ss);
